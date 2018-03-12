23c774eb48efc6fae27f1d77ace60fb535075531
at-quando <quan.do@asiantech.vn>
2018-03-12 16:17:39 +0700

    fix rroutes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   get "/bai-viet/:id" => 'links#about_house', :as => 'single_article_id'
   get "/tac-pham" => 'albums#index', :as => 'tac_pham'
   get "/giang-vien" => 'teachers#index'
-  get '/:name' => 'links#about_house', :as => 'single_article', :constraints => { :name => /(?!admin).*/ }
+  get '/:name' => 'links#about_house', :as => 'single_article', :constraints => { :name => /(?!(admin|home)).*/ }
   mount Spina::Engine   => '/'
 end
-
