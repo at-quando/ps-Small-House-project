3fcddbb1df7fa6eab013c77e38b2afafce9e8cd1
at-quando <quan.do@asiantech.vn>
2018-08-09 17:07:27 +0700

    fix error

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,6 @@
 class AlbumsController < ApplicationController
   def index
-    @works = Spina::Photo.where.not("student_id = ? or teacher_id = ?", nil, nil)
+    # @works = Spina::Photo.where.not("student_id = ? or teacher_id = ?", nil, nil)
+    @works = Spina::Photo.all
   end
 end
